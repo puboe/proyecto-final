d7475b33b3d614870f9c4a97814a9f483571d645
Pablo Diaz <pablodiazuboe@gmail.com>
2016-05-14 21:00:32 -0300

    Log to a file

--- ftp.py
+++ ftp.py
@@ -1,6 +1,7 @@
 import os
 from ftplib import FTP
 import time
+import logging
 
 FTP_SERVER = "goes.gsfc.nasa.gov"
 FTP_DIR = "/pub/goeseast/argentina/ir2/"
@@ -8,12 +9,14 @@ IMG_DIR = "images/"
 FILEMATCH = "*.tif"
 SCHEDULED_TIME = 30 * 60
 MIN_SIZE = 100 * 1024 # Min size in bytes
+LOG_FILE = "log/ftp-service.log"
 
 def fetch():
 	try:
+		logging.info("Connecting to server %s", FTP_SERVER)
 		ftp = FTP(FTP_SERVER)
 	except:
-		print "Couldn't find server"
+		logging.error("Couldn't find server %s", FTP_SERVER)
 		return
 	
 	ftp.login()
@@ -27,16 +30,21 @@ def fetch():
 			skipped += 1
 		else:
 			fhandle = open(os.path.join(IMG_DIR, filename), 'wb')
-			print 'Getting ' + filename
+			logging.info('Getting %s', filename)
 			ftp.retrbinary('RETR ' + filename, fhandle.write)
 			fhandle.close()
 			downloaded += 1
 
-	print str(downloaded) + " files downloaded, " + str(skipped) + " files skipped."
+	logging.info('%s files downloaded, %s files skipped."', downloaded, skipped)
 			
 	ftp.close()
 
+def config_logging():
+	logging.basicConfig(filename=LOG_FILE, level=logging.INFO, 
+		format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+
 def daemon():
+	config_logging()
 	while True:
 		fetch()
 		time.sleep(SCHEDULED_TIME)

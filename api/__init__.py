29bee833496505943c9892faad0cf07d4fc360db
Federico Pomar <fmpomar@gmail.com>
2016-07-13 01:00:56 -0300

    Static routes fix

--- api/__init__.py
+++ api/__init__.py
@@ -8,7 +8,10 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.url_map.converters['datetime'] = DateTimeConverter
 app.json_encoder = DateTimeJSONEncoder
 db = SQLAlchemy(app, session_options={'autocommit': False})
-blueprint = Blueprint('blueprint', __name__, url_prefix=app.config['APPLICATION_ROOT'])
+blueprint = Blueprint('blueprint', __name__,
+                      template_folder='templates',
+                      static_folder='static',
+                      url_prefix=app.config['APPLICATION_ROOT'])
 
 import api.views.zone
 import api.views.state

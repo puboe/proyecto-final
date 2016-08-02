ae70e9f6de7069c61f2557fc4132b02e6c680118
Federelli <federelli@gmail.com>
2016-08-02 12:29:04 -0300

    SavedStates view.

--- src/main/java/ar/com/itba/piedpiper/web/page/SavedStatesPage.java
+++ src/main/java/ar/com/itba/piedpiper/web/page/SavedStatesPage.java
@@ -1,5 +1,61 @@
 package ar.com.itba.piedpiper.web.page;
 
-public class SavedStatesPage {
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import ar.com.itba.piedpiper.model.entity.SavedState;
+import ar.com.itba.piedpiper.service.api.SavedStateService;
+import ar.com.itba.piedpiper.web.dataprovider.api.SpringPageDataProvider;
+
+@SuppressWarnings("serial")
+public class SavedStatesPage extends AbstractWebPage {
+
+	@SpringBean
+	private SavedStateService savedStates;
+
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		long itemsPerPage = 20;
+		IDataProvider<SavedState> savedStateDataProvider = new SpringPageDataProvider<SavedState>(itemsPerPage) {
+			@Override
+			protected Page<SavedState> getPage(Pageable pageable) {
+				return savedStates.findAll(pageable);
+			}
+
+			public void detach() {}
+		};
+		DataView<SavedState> savedStateView = new DataView<SavedState>("savedStateList", savedStateDataProvider) {
+			@Override
+			protected void populateItem(Item<SavedState> item) {
+				final SavedState savedState = (SavedState) item.getModelObject();
+				item.add(new Label("dateTime", savedState.dateTime()));
+				item.add(new Label("steps", savedState.steps()));
+				item.add(new Link<Void>("getState") {
+					@Override
+					public void onClick() {
+						
+					}
+				});
+				item.add(new Link<Void>("removeState") {
+					@Override
+					public void onClick() {
+						savedStates.delete(item.getModelObject());
+					}
+				});
+			}
+		};
+		savedStateView.setItemsPerPage(itemsPerPage);
+		add(new PagingNavigator("navigator", savedStateView));
+		add(new WebMarkupContainer("container").add(savedStateView));
+	}
 
 }
--- src/main/java/ar/com/itba/piedpiper/web/panel/PiedPiperNavbarBuilder.java
+++ src/main/java/ar/com/itba/piedpiper/web/panel/PiedPiperNavbarBuilder.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import ar.com.itba.piedpiper.web.NavbarBuilder;
 import ar.com.itba.piedpiper.web.page.EditConfigurationPage;
 import ar.com.itba.piedpiper.web.page.MainPage;
+import ar.com.itba.piedpiper.web.page.SavedStatesPage;
 import de.agilecoders.wicket.core.markup.html.bootstrap.image.GlyphIconType;
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar;
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.NavbarButton;
@@ -23,6 +24,7 @@ public class PiedPiperNavbarBuilder implements NavbarBuilder {
 		navbar.fluid();
 		navbar
 			.addComponents(transform(Navbar.ComponentPosition.LEFT, new NavbarButton<Void>(MainPage.class, Model.of("Main")).setIconType(GlyphIconType.cloud)))
+			.addComponents(transform(Navbar.ComponentPosition.LEFT, new NavbarButton<Void>(SavedStatesPage.class, Model.of("Saved States")).setIconType(GlyphIconType.floppysaved)))
 			.addComponents(transform(Navbar.ComponentPosition.LEFT, new NavbarButton<Void>(EditConfigurationPage.class, Model.of("Configuration")).setIconType(GlyphIconType.cog)));
 		return navbar;
 	}

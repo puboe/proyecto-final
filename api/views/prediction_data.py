b2e1054b0a45e992d3c12951e057fb42505dc5ef
Federico Pomar <fmpomar@gmail.com>
2016-08-02 23:52:53 -0300

    Image difference

--- api/views/prediction_data.py
+++ api/views/prediction_data.py
@@ -1,7 +1,8 @@
 from flask import jsonify, send_file, abort
 from api import blueprint, db
 from api.util import render_image_array
-from meteo.meteo_sql import MeteoPredictionData
+from meteo.meteo_sql import MeteoPredictionData, MeteoStaticData
+import numpy as np
 
 
 @blueprint.route('/<zone_name>/<datetime:time>/prediction/')
@@ -27,9 +28,16 @@ def prediction_data_image(zone_name, time):
 
 @blueprint.route('/<zone_name>/<datetime:time>/prediction/diff_image.png')
 def prediction_data_image_difference(zone_name, time):
-    data = db.session.query(MeteoPredictionData).filter_by(zone_name=zone_name,
+    prediction_data = db.session.query(MeteoPredictionData).filter_by(zone_name=zone_name,
                                                        time=time).first()
-    if data is None:
+    state_data = db.session.query(MeteoStaticData).filter_by(is_valid=True,
+                                                             zone_name=zone_name,
+                                                             channel='ir4',
+                                                             time=time).first()
+    if prediction_data is None or state_data is None:
         abort(404)
 
-    return render_image_array(data.image)
+    difference = np.abs(prediction_data.image - state_data.image)
+    difference = difference/np.max(difference)
+
+    return render_image_array(difference)
